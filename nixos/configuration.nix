d7f8c15f82fe5a436e2089b66f96d7f55cb0b1c8
vickoeff <vic.eff3@gmail.com>
2023-10-28 20:37:50 +0700

    remove manual installation of cloudflare-warp

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -129,12 +129,12 @@ in
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [ 
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     google-chrome
     whatsapp-for-linux
-    # cloudflare-warp
+    cloudflare-warp
   ];
 
   # cloudflare WARP
